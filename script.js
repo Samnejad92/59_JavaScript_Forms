8341f42586a6f16177012f0de44517e73aacbdb6
samnejad92 <m.samnejad92@gmail.com>
2024-07-20 18:00:09 +0330

    input validation

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 	document.getElementById('cart-hplus')
     .addEventListener('submit',function(event){
         event.preventDefault();
+        if(document.getElementById('s-state').value===''){
+            alert('Choose state');
+            document.getElementById('s-state').focus();
+        }
     })();
 	
 	
